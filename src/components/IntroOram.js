06d57c6355edb6e79c037ff65bb94ff2668b7695
elaine <elaine@localhost.localdomain>
2024-05-27 21:21:59 -0400

    minor

--- src/components/IntroOram.js
+++ src/components/IntroOram.js
@@ -14,7 +14,7 @@ function IntroOram() {
 but also the accesses to data." />
                     </div>
                     <div className="col-md-3 mb-2">
-                    <Card title="Our Mission" img={card2} text="We provide open source implementations of Oblivious RAM and oblivious algorithms. Check out our
+                    <Card title="Open source" img={card2} text="We provide open source implementations of Oblivious RAM and oblivious algorithms. Check out our
 open source releases on <a href=\"https://github.com/obliviouslabs/oram\">Github </a>." />
                     </div>
 
