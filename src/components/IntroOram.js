0fec7e143eebc13efbbd21a2a0b83e38ed303394
GUA <songjunxi@GUA.local>
2024-05-28 10:33:48 +0800

    swap the img for card 1 and 2 in intro to oram

--- src/components/IntroOram.js
+++ src/components/IntroOram.js
@@ -10,10 +10,10 @@ function IntroOram() {
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-3 mb-2">
-                    <Card title="What is ORAM?" img={card1} text="Oblivious RAM (ORAM) provides a secret database that 'encrypts' not only the data contents, but also the accesses to data." />
+                    <Card title="What is ORAM?" img={card2} text="Oblivious RAM (ORAM) provides a secret database that 'encrypts' not only the data contents, but also the accesses to data." />
                     </div>
                     <div className="col-md-3 mb-2">
-                    <Card title="Applications of ORAM" img={card2} text="Our Path ORAM algorithm is used by the <a href='https://signal.org/blog/building-faster-oram/'>Signal encrypted messenger</a> for private contact discovery. ORAM can also be used in blockchains to enable private order flow and mitigate Miner Extractable Value (MEV)." />
+                    <Card title="Applications of ORAM" img={card1} text="Our Path ORAM algorithm is used by the <a href='https://signal.org/blog/building-faster-oram/'>Signal encrypted messenger</a> for private contact discovery. ORAM can also be used in blockchains to enable private order flow and mitigate Miner Extractable Value (MEV)." />
                     </div>
 <div className="col-md-3 mb-2">
                     <Card title="Open source" img={card3} text="We provide open source implementations of Oblivious RAM and oblivious algorithms. Check out our
