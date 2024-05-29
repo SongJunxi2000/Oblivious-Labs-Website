a1e5fcc07049c0df527321f2a01aaed53473d5a0
GUA <songjunxi@GUA.local>
2024-05-29 21:17:44 +0800

    update Social Media in footer for github and twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,13 +13,26 @@ function Footer() {
                 <h1 className="logo">Oblivious Labs</h1>
                 <p className="footer-text">
                   Private and Efficient Access to Database with ORAM Technology
-        
                 </p>
               </div>
-              
             </div>
           </div>
-          
+          <div className="side2">
+            <div className="row">
+              <div className="col-md-3">
+                <p className="footer-title">Social Media</p>
+                <ul>
+                  <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/obliviouslabs/oram" > Github</a>
+                  </li>
+                  <li>
+                    <a target="_blank" rel="noreferrer" href="https://x.com/obliviouslabs" > Twitter</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <button onClick={() => scroll.scrollToTop(2500)} src="" className="gotop"><i className="fas fa-level-up-alt"></i></button>
