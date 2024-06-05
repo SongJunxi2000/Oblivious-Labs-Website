4fd4b79412a6055f79f6918f553383bcb7c53985
GUA <songjunxi@GUA.local>
2024-06-05 21:38:40 +0800

    update Elaine's title

--- src/components/Team.js
+++ src/components/Team.js
@@ -11,7 +11,7 @@ function TeamMember() {
                 <div className="row">
                     <div className="col-md-3 mb-2">
                     <Card title="Elaine Shi" text="<div class='text-center'>
-    President and Chief Scientist <br><br>
+    President<br> Chief Scientist <br><br>
   </div>" />
                     </div>
                     <div className="col-md-3 mb-2">
