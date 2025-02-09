a8533fc9891b667bd006bb878da49b3cec872e90
GUA <songjunxi@gua.mynetworksettings.com>
2025-02-09 13:09:40 -0800

    NSF SBIR logo added

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,4 +1,5 @@
 import flashbots from "./img/Flashbots-logo.svg";
+import NSF from "./img/NSFSBIR.png";
 
 function Sponsor() {
   return (
@@ -6,11 +7,18 @@ function Sponsor() {
       <h2 className="main-title">Sponsors</h2>
       
       {/* Inline style to set a max width */}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
       <img 
         src={flashbots} 
         alt="Flashbots logo" 
         style={{ maxWidth: "200px", height: "auto" }} 
       />
+      <img 
+        src={NSF} 
+        alt="NSF SBIR logo" 
+        style={{ maxWidth: "200px", height: "auto" }} 
+      />
+      </div>
     </div>
   );
 }
