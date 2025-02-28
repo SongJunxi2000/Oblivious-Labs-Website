2d16d0c5ed5bedc8640351c3aa006aff734a2cee
GUA <songjunxi@gua.mynetworksettings.com>
2025-02-28 11:41:15 -0800

    add the warning for not supported on mobile

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -5,6 +6,22 @@ import BalanceChecker from "./components/BalanceChecker";
 import Footer from "./components/Footer";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20%" }}>
+        <h2>This website is not supported on mobile. Please use a laptop or desktop.</h2>
+      </div>
+    );
+  }
+
   return (
     <>
       <Navbar />
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,37 +1,38 @@
 import Card from "./Card";
-import card1 from  "./img/card1.png";
-import card2 from  "./img/card2.png";
-import card3 from  "./img/card3.png";
 function TeamMember() {
   return (
     <div className="container team-scroll">
         <h2 className="main-title text-center">Team</h2>
         <div className="card-cover">
             <div className="col-md-12">
-                <div className="row">
-                    <div className="col-md-6-center">
+            <div className="row">
+                    <div className="col-md-3">
                     <Card title="Elaine Shi" text="<div class='text-center'>
     President<br> Chief Scientist <br><br>
   </div>" />
                     </div>
-                    <div className="col-md-6-center">
+                    <div className="col-md-3">
                     <Card title="Andrew Miller" text="<div class='text-center'>
     Advisor  <br><br>
   </div>" />
                     </div>
                 </div>
+            </div>
+        </div>
+        <div className="card-cover">
+            <div className="col-md-12">
                 <div className="row">
-                    <div className="col-md-4 mb-2">
+                    <div className="col-md-3 mb-2">
                     <Card title="Afonso Tinoco" text="<div class='text-center'>
     Co-Founder<br> Lead Research Engineer  <br><br>
   </div>" />
                         </div>
-                    <div className="col-md-4 mb-2">
+                    <div className="col-md-3 mb-2">
                     <Card title="Tianyao Gu" text="<div class='text-center'>
     Co-Founder<br>   <br><br>
   </div>" />
                       </div>
-                    <div className="col-md-4 mb-2">
+                    <div className="col-md-3 mb-2">
                     <Card title="Junxi Song" text="<div class='text-center'>
     Co-Founder<br> Research Engineer  <br><br>
   </div>" />
@@ -39,6 +40,8 @@ function TeamMember() {
                 </div>
             </div>
         </div>
+
+
     </div>
   );
 }
